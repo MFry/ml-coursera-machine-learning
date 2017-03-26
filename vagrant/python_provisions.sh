97ea36d4cf028a66ab47a9ddc233e640c4f2f7a7
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-03-26 15:12:40 -0800

    Chore: Added python3.6 support with virtualenv

---
+++ vagrant/python_provisions.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#Thanks to Dwayne. | Link: https://gist.github.com/dwayne/87f807f0d313b444bb37
+set -e
+echo 'Running Python Provisions'
+sudo apt-get -qq install libpq-dev python3-dev
+apt-get -qq install libpq-dev libreadline-dev libsqlite3-dev libssl-dev
+cd /tmp
+wget -O- -q https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz | tar xz
+cd Python-3.6.1
+./configure
+make -s
+make -s altinstall
+sudo -H pip3.6 install --upgrade pip
+sudo -H pip3.6 -qq install python-virtualenv
+sudo -H pip3.6 install virtualenvwrapper
+virtualenv vagrant/dev/virtualenv/ml
+
+echo 'Clean up...'
+cd && rm -rf /tmp/Python-3.6.1
