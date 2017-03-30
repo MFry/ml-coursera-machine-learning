d85b5fd14c737c9a3de3a125444ee13d17a816e4
Michal Frystacky <Michal.Frystacky@gmail.com>
2017-03-29 22:13:30 -0800

    Fix: Fixing Virtualenv install for python 3.6

--- vagrant/python_provisions.sh
+++ vagrant/python_provisions.sh
@@ -4,7 +4,7 @@
 set -e
 echo 'Running Python Provisions'
 sudo apt-get -qq install libpq-dev python3-dev
-apt-get -qq install libpq-dev libreadline-dev libsqlite3-dev libssl-dev
+sudo apt-get -qq install libpq-dev libreadline-dev libsqlite3-dev libssl-dev
 cd /tmp
 wget -O- -q https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz | tar xz
 cd Python-3.6.1
@@ -12,7 +12,7 @@ cd Python-3.6.1
 make -s
 make -s altinstall
 sudo -H pip3.6 install --upgrade pip
-sudo -H pip3.6 -qq install python-virtualenv
+sudo -H pip3.6 -qq install python3-virtualenv
 sudo -H pip3.6 install virtualenvwrapper
 virtualenv vagrant/dev/virtualenv/ml
 
